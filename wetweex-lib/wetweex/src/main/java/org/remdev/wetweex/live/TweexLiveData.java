b6c01e4fe1f666433350a8d74deb48b4170e38b9
Sergey Nalivko <snalivko93@gmail.com>
2019-06-05 16:43:59 +0300

    [WT-8] Removing redundant objects, moving observe method to TweexLiveData

--- wetweex-lib/wetweex/src/main/java/org/remdev/wetweex/live/TweexLiveData.java
+++ wetweex-lib/wetweex/src/main/java/org/remdev/wetweex/live/TweexLiveData.java
@@ -1,6 +1,12 @@
 package org.remdev.wetweex.live;
 
+import androidx.lifecycle.LifecycleOwner;
+import androidx.lifecycle.Observer;
+
 public interface TweexLiveData<T> {
 
     T getValue();
+
+    void observe(LifecycleOwner owner, Observer<? super T> data);
+
 }
--- wetweex-lib/wetweex/src/main/java/org/remdev/wetweex/live/TweexMutableLiveData.java
+++ wetweex-lib/wetweex/src/main/java/org/remdev/wetweex/live/TweexMutableLiveData.java
@@ -1,11 +1,6 @@
 package org.remdev.wetweex.live;
 
-import androidx.lifecycle.LifecycleOwner;
-import androidx.lifecycle.Observer;
-
 public interface TweexMutableLiveData<T> extends TweexLiveData<T> {
 
     void setOrPost(T data);
-
-    void observe(LifecycleOwner owner, Observer<? super T> data);
 }
